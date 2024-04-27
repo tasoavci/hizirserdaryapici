bb5ce64b7b7d2f75400e1e741f5e01aa8bf829b4
Tahsin Avci <99720123+tasoavci@users.noreply.github.com>
2024-04-27 22:17:30 +0300

    updated

--- admin/portfolio.php
+++ admin/portfolio.php
@@ -111,19 +111,19 @@
                                                 <tr>
                                                     <td>
                                                         <?php if($portfolio['Portfolio_Type']=="images"){ ?>
-                                                            <img style="width: 70%; margin-bottom: 2%;" src="<?php echo 'https://hizirserdaryapici.com/assets/img/portfolio/'.$portfolio['Portfolio_Frontimg'];?>"/>
+                                                            <img style="width: 70%; margin-bottom: 2%;" src="<?php echo 'assets/img/portfolio/'.$portfolio['Portfolio_Frontimg'];?>"/>
                                                         <?php }else{ ?>
                                                             <video style="width: 70%; margin-bottom: 2%;" controls>
-                                                                <source src="<?php echo 'https://hizirserdaryapici.com/assets/img/portfolio/'.$portfolio['Portfolio_Frontimg'];?>">
+                                                                <source src="<?php echo 'assets/img/portfolio/'.$portfolio['Portfolio_Frontimg'];?>">
                                                             </video>
                                                         <?php } ?>
                                                     </td>
                                                     <td>
                                                         <?php if($portfolio['Portfolio_Type']=="images"){ ?>
-                                                            <img style="width: 35%; margin-bottom: 2%;" src="<?php echo 'https://hizirserdaryapici.com/assets/img/SERDAR_PORTFOLIO/'.$portfolio['Portfolio_Magnificationimg'];?>"/>
+                                                            <img style="width: 35%; margin-bottom: 2%;" src="<?php echo 'assets/img/SERDAR_PORTFOLIO/'.$portfolio['Portfolio_Magnificationimg'];?>"/>
                                                         <?php }else{ ?>
                                                             <video style="width: 70%; margin-bottom: 2%;" controls>
-                                                                <source src="<?php echo 'https://hizirserdaryapici.com/assets/img/SERDAR_PORTFOLIO/'.$portfolio['Portfolio_Magnificationimg'];?>">
+                                                                <source src="<?php echo 'assets/img/SERDAR_PORTFOLIO/'.$portfolio['Portfolio_Magnificationimg'];?>">
                                                             </video>
                                                         <?php } ?>
                                                         
